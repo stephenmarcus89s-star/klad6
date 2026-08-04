05507a3e98c34c0c450c8f1dd9d5de6e5c89c616
Z User <z@container>
2026-08-04 07:47:54 +0000

    Force Render redeploy for v7.9.6 admin panel

--- admin-panel/app.js
+++ admin-panel/app.js
@@ -8742,3 +8742,4 @@ async function animeDeleteEpisode(epId) {
 // v7.9.2 deploy trigger Mon Aug  3 12:10:55 UTC 2026
 // v7.9.2 deploy Mon Aug  3 13:11:12 UTC 2026
 // v7.9.3 deploy 1785767108
+// v7.9.6 deploy 1785829674
